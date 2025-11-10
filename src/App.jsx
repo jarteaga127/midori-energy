170967d728d1ff5795bcb9954507ce9480639472
Joseph Arteaga <jarteaga127@gmail.com>
2025-11-10 12:33:57 +0900

    Created routes for the home page and about us page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import { Route, Routes } from "react-router-dom"
 import Footer from "./components/Footer"
 import Navbar from "./components/Navbar"
+import AboutPage from "./pages/AboutPage"
 import HomePage from "./pages/HomePage"
 
 function App() {
@@ -8,7 +10,11 @@ function App() {
   return (
     <>
       <Navbar/>
-      <HomePage/>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+      <Route path="/about-us" element={<AboutPage/>} />
+      
+      </Routes>
       <Footer/>
     </>
   )
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,10 @@
+const AboutPage = () => {
+    return ( 
+        <main>
+            <h1>About Us</h1>
+            <p>Kyoto ya Nara ni wa Nihon no furui bunka ga nokotteiru. Kyoto, Nara wo otozureru hitobito ga rekishi wo jibun no me de miru koto ga dekiru. Shikashi, mae motte rekishi wo shitteireba, Kyoto ya Nara nado wo otozureru kachi ga motto fueru to omou.</p>
+        </main>
+     );
+}
+ 
+export default AboutPage;
