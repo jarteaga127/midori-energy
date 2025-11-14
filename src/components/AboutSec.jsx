93c5cead3ab9840ea0df4659923b37d7b1f8e2cf
Joseph Arteaga <jarteaga127@gmail.com>
2025-11-14 12:40:13 +0900

    Made slight edits to the hero section

--- src/components/AboutSec.jsx
+++ src/components/AboutSec.jsx
@@ -1,10 +1,13 @@
 const AboutSec = () => {
     return ( 
         <section className="about-sec">
+            <div className="about-sec-img"></div>
+            <div className="about-sec-text">
             <h2>About us</h2>
             <p>Kyoto ya Nara ni wa Nihon no furui bunka ga nokotteiru. Kyoto, Nara wo otozureru hitobito ga rekishi wo jibun no me de miru koto ga dekiru. Shikashi, mae motte rekishi wo shitteireba, Kyoto ya Nara wo otozureru kachi ga motto fueru to omou.
             </p>
             <button>Read more about our story here.</button>
+            </div>
         </section>
      );
 }
--- src/components/HeroSec.jsx
+++ src/components/HeroSec.jsx
@@ -4,8 +4,10 @@ const HeroSec = () => {
     return ( 
         <>
         <section className="hero-sec">
+            <div className="hero-sec-text">
             <h1>Add more green to your life. Drink Midori.</h1>
             <button>Shop now</button>
+            </div>
         </section>
         </>
      );
