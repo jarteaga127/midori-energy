b4027ee795d600a5fe47eddb14214ba9cd866ba4
Joseph Arteaga <jarteaga127@gmail.com>
2025-11-18 12:28:44 +0900

    Made edits to the featured section

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,6 +1,10 @@
+import '../styles/features.css'
+
 const Features = () => {
     return ( 
         <section className="feature-sec">
+            <div className="feature-img"></div>
+            <div className="feature-text">
             <h2>What drinking Midori Green Tea can do for you?</h2>
             <p>
 Kyoto ya Nara ni wa Nihon no furui bunka ga nokotteiru. Kyoto, Nara wo otozureru hitobito ga rekishi wo jibun no me de miru koto ga dekiru. Shikashi, mae motte rekishi wo shitteireba, Kyoto ya Nara nado wo otozureru kachi ga motto fueru to omou.
@@ -13,6 +17,7 @@ Kyoto ya Nara ni wa Nihon no furui bunka ga nokotteiru. Kyoto, Nara wo otozureru
                 <li>No high fructose corn syrup or other sweeteners we cannot pronounce</li>
             </ul>
             <button>See our list of ingredients</button>
+            </div>
         </section>
      );
 }
