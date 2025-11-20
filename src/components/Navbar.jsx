963ffda35b8da0eff76cefb77a491e3602a322d9
Joseph Arteaga <jarteaga127@gmail.com>
2025-11-20 12:31:24 +0900

    Installed react-icons and added side menu button to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import '../styles/navbar.css'
+import { FaBars } from "react-icons/fa6";
 
 const Navbar = () => {
     return ( 
@@ -13,6 +14,7 @@ const Navbar = () => {
                 <li>Subscribe</li>
                 <li>Login</li>
             </ul>
+            <button className="side-menu-btn"><FaBars/></button>
         </nav>
         </>
      );
