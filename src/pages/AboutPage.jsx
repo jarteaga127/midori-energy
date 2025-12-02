f751a8297923ff088227219e5fb696e79ba157b3
Joseph Arteaga <jarteaga127@gmail.com>
2025-12-02 12:34:12 +0900

    Edited the css files for different sections

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,9 +1,9 @@
 const AboutPage = () => {
     return ( 
-        <main>
+        <section>
             <h1>About Us</h1>
             <p>Kyoto ya Nara ni wa Nihon no furui bunka ga nokotteiru. Kyoto, Nara wo otozureru hitobito ga rekishi wo jibun no me de miru koto ga dekiru. Shikashi, mae motte rekishi wo shitteireba, Kyoto ya Nara nado wo otozureru kachi ga motto fueru to omou.</p>
-        </main>
+        </section>
      );
 }
  
