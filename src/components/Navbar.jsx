a279678b4d5a7520d5ee27a8ff1c6af47faffc32
Joseph Arteaga <jarteaga127@gmail.com>
2025-12-05 12:39:36 +0900

    Edited css file for navbar to fit iPad screens

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import { Link } from 'react-router-dom';
-import '../styles/navbar.css'
+import '../styles/navbar.css';
+import { RiCloseLargeFill } from "react-icons/ri";
 import { FaBars } from "react-icons/fa6";
 
 const Navbar = ({toggle, isOpen}) => {
 
    const containerClass = isOpen ? "side-menu-cont open" : "side-menu-cont";
+
+   const sideMenuIcon = isOpen ? <RiCloseLargeFill/> : <FaBars/>;
 
 
     return ( 
@@ -19,7 +22,7 @@ const Navbar = ({toggle, isOpen}) => {
                 <li>Login</li>
             </ul>
             
-            <div className="side-menu-btn" onClick={toggle}><FaBars/></div>
+            <div className="side-menu-btn" onClick={toggle}>{sideMenuIcon}</div>
             
         </nav>
         <div className={containerClass} >
