239d572aec124b7a7e218f76ec797246c5409b32
Joseph Arteaga <jarteaga127@gmail.com>
2025-12-18 12:24:59 +0900

    Wrote a unit test to check for links in the navbar

---
+++ tests/Navbar.test.jsx
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import NavBar from "../src/components/Navbar";
+import { MemoryRouter } from "react-router-dom";
+import {describe, it, expect} from "vitest";
+
+
+describe ('Navbar', () => {
+    it('should have three links in the navbar', () => {
+        render(
+        <MemoryRouter>
+        <NavBar/>
+    </MemoryRouter>
+    );
+        const navLinks = screen.getAllByRole('link');
+        expect(navLinks).toHaveLength(5);
+
+    }); 
+});
