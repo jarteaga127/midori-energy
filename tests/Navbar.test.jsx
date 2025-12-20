2d651408f8c039a5c0014af749aeaa87360a5dc1
Joseph Arteaga <jarteaga127@gmail.com>
2025-12-20 21:52:38 +0900

    Added a few more tests for the navbar component

--- tests/Navbar.test.jsx
+++ tests/Navbar.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import NavBar from "../src/components/Navbar";
 import { MemoryRouter } from "react-router-dom";
 import {describe, it, expect} from "vitest";
@@ -15,4 +15,28 @@ describe ('Navbar', () => {
         expect(navLinks).toHaveLength(5);
 
     }); 
+
+it('should have links that goes to the home page', () => {
+     render(
+        <MemoryRouter>
+        <NavBar/>
+    </MemoryRouter>
+    );
+    const allLinks = screen.getAllByRole('link');
+    const homeLink = allLinks.find(link => link.getAttribute('href') === '/');
+    expect(homeLink).toBeTruthy();
+})
+
+it('should toggle the side menu when the hamburger button is clicked', () => {
+        const mockToggle = vi.fn();
+        render(
+            <MemoryRouter>
+        <NavBar toggle={mockToggle} isOpen={false}/>
+    </MemoryRouter>
+        )
+        const sideMenuBtn = screen.getByTestId('side-menu-toggle');
+        fireEvent.click(sideMenuBtn);
+        expect(mockToggle).toHaveBeenCalledTimes(1);
+    })
+    
 });
