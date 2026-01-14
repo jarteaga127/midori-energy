7a0d31bcf041367fc45f5b8c90e0fb71e4df6fb2
Joseph Arteaga <jarteaga127@gmail.com>
2026-01-14 12:52:05 +0900

    Edited styles for the Login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,18 +2,19 @@ import '../styles/login-page.css'
 
 const LoginPage = () => {
     return ( 
-        <>
+        <section>
             <h1>Please Login</h1>
             <form action="">
                 <label htmlFor="username">Username:</label>
                 <input type="text" id="username"/>
                 <label htmlFor="password">Password:</label>
                 <input type="password" />
+                <input type="submit" value="Login" className='login-btn' />
             </form>
             <div className="make-account-sec">
                 <p>Don't have an account? Let's make one!</p>
             </div>
-        </>
+        </section>
      );
 }
  
