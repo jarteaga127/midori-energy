c64c57303aef7a3048e657fb6d347e686e5abe66
Joseph Arteaga <jarteaga127@gmail.com>
2026-04-16 12:25:31 +0900

    Edited styles for the section elements

--- src/components/AboutSec.jsx
+++ src/components/AboutSec.jsx
@@ -4,13 +4,13 @@ import '../styles/about-sec.css';
 const AboutSec = () => {
     return ( 
         <section className="about-sec">
-            <div className="about-sec-img"></div>
             <div className="about-sec-text">
             <h2>About us</h2>
             <p>Kyoto ya Nara ni wa Nihon no furui bunka ga nokotteiru. Kyoto, Nara wo otozureru hitobito ga rekishi wo jibun no me de miru koto ga dekiru. Shikashi, mae motte rekishi wo shitteireba, Kyoto ya Nara wo otozureru kachi ga motto fueru to omou.
             </p>
                 <Link to="about-us" className='section-link'>Read more about our story here.</Link>
             </div>
+             <div className="about-sec-img"></div>
         </section>
      );
 }
