12d717b44a892ceec0bf9ae7d697a720175b72d4
Joseph Arteaga <jarteaga127@gmail.com>
2026-05-15 12:27:36 +0900

    Edited styles for the subscribe section

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -1,13 +1,15 @@
+import "../styles/subscribe.css";
+
 const Subscribe = () => {
     return ( 
-        <section>
+        <section className="subscribe-section">
            <div className="subscribe-container">
             <div className="sub-cont-text">
             <h1>Sign up for our newsletter!</h1>
             <p>Stay up to date for our laterst products, flavors and where we'll pop up next.</p>
             </div>
             <div className="sub-form-container">
-                <form className="newsletter-form">
+                <form className="subscribe-form">
                     <input type="email" placeholder="Enter your email here" />
                     <button type="submit">Subscribe</button>
                 </form>
